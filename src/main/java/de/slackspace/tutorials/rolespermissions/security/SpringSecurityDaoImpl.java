86397a2ca0264195804600592a986d562b638d48
cte <cte@slackspace.de>
2011-07-20 21:32:26 +0200

    Initial commit

---
+++ src/main/java/de/slackspace/tutorials/rolespermissions/security/SpringSecurityDaoImpl.java
@@ -0,0 +1,7 @@
+package de.slackspace.tutorials.rolespermissions.security;
+
+import org.springframework.security.core.userdetails.jdbc.JdbcDaoImpl;
+
+public class SpringSecurityDaoImpl extends JdbcDaoImpl {
+
+}
---
+++ src/main/java/de/slackspace/tutorials/rolespermissions/startup/TestDataProvider.java
@@ -0,0 +1,74 @@
+package de.slackspace.tutorials.rolespermissions.startup;
+
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.InitializingBean;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Component;
+
+/**
+ * A class just to create the database tables and provide some test users.
+ */
+@Component
+public class TestDataProvider implements InitializingBean {
+
+	@Autowired
+	private DataSource dataSource;
+	
+	@Override
+	public void afterPropertiesSet() throws Exception {
+		JdbcTemplate jdbcTemplate = new JdbcTemplate(dataSource);
+		
+		deleteExistingData(jdbcTemplate);
+		createTables(jdbcTemplate);
+		createData(jdbcTemplate);
+	}
+
+	private void deleteExistingData(JdbcTemplate jdbcTemplate) {
+		jdbcTemplate.execute("drop table role_members");
+		jdbcTemplate.execute("drop table role_permissions");
+		jdbcTemplate.execute("drop table permissions");
+		jdbcTemplate.execute("drop table roles");
+		jdbcTemplate.execute("drop table users");
+	}
+
+	private void createData(JdbcTemplate jdbcTemplate) {
+		jdbcTemplate.execute("insert into users(username, password, enabled) VALUES('rod', 'koala', true)");;
+		jdbcTemplate.execute("insert into permissions(name) VALUES('Show Account Page')");
+		jdbcTemplate.execute("insert into roles(name) VALUES('Admin')");
+		jdbcTemplate.execute("insert into role_members(roles_id, members_id) VALUES(1,1)");
+		jdbcTemplate.execute("insert into role_permissions(roles_id,permissions_id) VALUES(1,1)");
+		
+		jdbcTemplate.execute("insert into users(username, password, enabled) VALUES('peter', 'opal', true)");;
+		jdbcTemplate.execute("insert into permissions(name) VALUES('Show Protected Page')");
+		jdbcTemplate.execute("insert into roles(name) VALUES('Users')");
+		jdbcTemplate.execute("insert into role_members(roles_id, members_id) VALUES(2,2)");
+		jdbcTemplate.execute("insert into role_permissions(roles_id,permissions_id) VALUES(2,2)");
+	}
+	
+	private void createTables(JdbcTemplate jdbcTemplate) {
+		jdbcTemplate.execute("create table if not exists users("+
+				  "id bigint AUTO_INCREMENT not null primary key,"+
+				  "username varchar(100) not null,"+
+				  "password varchar(100) not null,"+
+				  "enabled boolean not null);");
+							      
+		jdbcTemplate.execute("create table if not exists roles("+
+				  "id bigint AUTO_INCREMENT not null primary key,"+
+				  "name varchar(100) not null);");
+				  
+		jdbcTemplate.execute("create table if not exists permissions("+
+				  "id bigint AUTO_INCREMENT not null primary key,"+
+				  "name varchar(100) not null);");
+				  
+		jdbcTemplate.execute("create table if not exists role_members("+
+				  "roles_id bigint not null,"+
+				  "members_id bigint not null);");
+
+		jdbcTemplate.execute("create table if not exists role_permissions("+
+				  "roles_id bigint not null,"+
+				  "permissions_id bigint not null);");
+	}
+
+}
